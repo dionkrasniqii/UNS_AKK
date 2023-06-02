6de6b3566e35030e9d8210fa113e08b855330d39
Qendrim Pllana <qendrimpllana27@gmail.com>
2023-06-02 14:52:49 +0200

    .

--- uns_akk/src/components/agencies/Agencies.jsx
+++ uns_akk/src/components/agencies/Agencies.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import DataTable from "../../datatable/DataTable";
 
 export default function Agencies() {
   return (
     <div className="col-xxl-12">
       <div className="col-xxl-12 text-end">
-        <Link class="btn btn-info waves-effect waves-light" to="/createagencies">
+        <Link
+          class="btn btn-info waves-effect waves-light"
+          to="/createagencies"
+        >
           <span class="btn-label">
             <i class="mdi mdi-alert-circle-outline"></i>
           </span>
           Shto
         </Link>
       </div>
+      <DataTable />
     </div>
   );
 }
