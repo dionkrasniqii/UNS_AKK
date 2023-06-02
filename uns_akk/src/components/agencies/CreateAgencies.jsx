978a4e00ad18d2856abc90d5f741b996b377b1d3
Qendrim Pllana <qendrimpllana27@gmail.com>
2023-06-02 14:42:59 +0200

    .

--- uns_akk/src/components/agencies/CreateAgencies.jsx
+++ uns_akk/src/components/agencies/CreateAgencies.jsx
@@ -1,21 +1,46 @@
 import { useFormik } from "formik";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { CreateAgenciesSchema } from "../schemas/CreateAgenciesSchema";
+import { object } from "yup";
+import { red } from "@mui/material/colors";
 
 export default function CreateAgencies() {
   const [model, setModel] = useState({
     Name: "",
+    UniqueNumber: "",
     City: "",
+    Address: "",
     PostalCode: "",
+    PhoneNumber: "",
     Email: "",
     Web: "",
-    Address: "",
+    Documents: "",
   });
   const [progessBarWidth, setProgressBarWidth] = useState(0);
+  const [progressColor, setProgressColor] = useState("");
+
+  useEffect(() => {
+    const percentage = 100;
+    const totalKeys = Object.keys(model).length;
+    const nullValuesCount = Object.values(model).filter(
+      (value) => !value
+    ).length;
+    const progress =
+      nullValuesCount === 0
+        ? percentage
+        : (percentage / totalKeys) * (totalKeys - nullValuesCount);
+    const progressColor =
+      progress <= 50
+        ? "bar progress-bar progress-bar-striped progress-bar-animated bg-danger"
+        : progress > 50 && progress < 75
+        ? "bar progress-bar progress-bar-striped progress-bar-animated bg-warning"
+        : "bar progress-bar progress-bar-striped progress-bar-animated bg-success";
+    setProgressBarWidth(`${progress}%`);
+    setProgressColor(progressColor);
+  }, [model]);
 
   async function SubmitForm() {}
-
   const formik = useFormik({
     initialValues: {},
     validationSchema: CreateAgenciesSchema,
@@ -32,7 +57,7 @@ export default function CreateAgencies() {
               <div className="tab-content b-0 mb-0 pt-0">
                 <div id="bar" className="progress mb-3" style={{ height: 7 }}>
                   <div
-                    className="bar progress-bar progress-bar-striped progress-bar-animated bg-success"
+                    className={progressColor}
                     style={{ width: progessBarWidth }}
                   />
                 </div>
@@ -40,11 +65,8 @@ export default function CreateAgencies() {
                   <div className="row">
                     <div className="col-12">
                       <div className="row mb-3">
-                        <label
-                          className="col-md-3 col-form-label"
-                          htmlFor="userName1"
-                        >
-                          Name of institution
+                        <label className="col-md-3 col-form-label">
+                          Name of Institution
                         </label>
                         <div className="col-md-9">
                           <input
@@ -67,12 +89,31 @@ export default function CreateAgencies() {
                         </div>
                       </div>
                       <div className="row mb-3">
-                        <label
-                          className="col-md-3 col-form-label"
-                          htmlFor="password1"
-                        >
-                          City
+                        <label className="col-md-3 col-form-label">
+                          Unique Number
                         </label>
+                        <div className="col-md-9">
+                          <input
+                            type="number"
+                            onChange={(e) => {
+                              setModel({
+                                ...model,
+                                UniqueNumber: e.target.value,
+                              });
+                              formik.setFieldValue("UniqueNumber", e.target.value);
+                            }}
+                            className="form-control"
+                          />
+                          {formik.errors.UniqueNumber && (
+                            <span className="text-danger">
+                              {" "}
+                              {formik.errors.UniqueNumber}
+                            </span>
+                          )}
+                        </div>
+                      </div>
+                      <div className="row mb-3">
+                        <label className="col-md-3 col-form-label">City</label>
                         <div className="col-md-9">
                           <input
                             type="text"
@@ -81,76 +122,20 @@ export default function CreateAgencies() {
                                 ...model,
                                 City: e.target.value,
                               });
+                              formik.setFieldValue("City", e.target.value);
                             }}
                             className="form-control"
                           />
+                          {formik.errors.City && (
+                            <span className="text-danger">
+                              {" "}
+                              {formik.errors.City}
+                            </span>
+                          )}
                         </div>
                       </div>
                       <div className="row mb-3">
-                        <label
-                          className="col-md-3 col-form-label"
-                          htmlFor="confirm1"
-                        >
-                          PostalCode
-                        </label>
-                        <div className="col-md-9">
-                          <input
-                            type="number"
-                            className="form-control"
-                            onChange={(e) => {
-                              setModel({
-                                ...model,
-                                PostalCode: e.target.value,
-                              });
-                            }}
-                          />
-                        </div>
-                      </div>
-                      <div className="row mb-3">
-                        <label
-                          className="col-md-3 col-form-label"
-                          htmlFor="password1"
-                        >
-                          Email
-                        </label>
-                        <div className="col-md-9">
-                          <input
-                            type="email"
-                            onChange={(e) => {
-                              setModel({
-                                ...model,
-                                Email: e.target.value,
-                              });
-                            }}
-                            className="form-control"
-                          />
-                        </div>
-                      </div>
-                      <div className="row mb-3">
-                        <label
-                          className="col-md-3 col-form-label"
-                          htmlFor="password1"
-                        >
-                          Web
-                        </label>
-                        <div className="col-md-9">
-                          <input
-                            type="text"
-                            onChange={(e) => {
-                              setModel({
-                                ...model,
-                                Web: e.target.value,
-                              });
-                            }}
-                            className="form-control"
-                          />
-                        </div>
-                      </div>
-                      <div className="row mb-3">
-                        <label
-                          className="col-md-3 col-form-label"
-                          htmlFor="password1"
-                        >
+                        <label className="col-md-3 col-form-label">
                           Address
                         </label>
                         <div className="col-md-9">
@@ -161,9 +146,132 @@ export default function CreateAgencies() {
                                 ...model,
                                 Address: e.target.value,
                               });
+                              formik.setFieldValue("Address", e.target.value);
                             }}
                             className="form-control"
                           />
+                          {formik.errors.Address && (
+                            <span className="text-danger">
+                              {" "}
+                              {formik.errors.Address}
+                            </span>
+                          )}
+                        </div>
+                      </div>
+                      <div className="row mb-3">
+                        <label className="col-md-3 col-form-label">
+                          Postal Code
+                        </label>
+                        <div className="col-md-9">
+                          <input
+                            type="number"
+                            className="form-control"
+                            onChange={(e) => {
+                              setModel({
+                                ...model,
+                                PostalCode: e.target.value,
+                              });
+                              formik.setFieldValue("PostalCode", e.target.value);
+                            }}
+                          />
+                          {formik.errors.PostalCode && (
+                            <span className="text-danger">
+                              {" "}
+                              {formik.errors.PostalCode}
+                            </span>
+                          )}
+                        </div>
+                      </div>
+                      <div className="row mb-3">
+                        <label className="col-md-3 col-form-label">
+                          Phone Number
+                        </label>
+                        <div className="col-md-9">
+                          <input
+                            type="text"
+                            className="form-control"
+                            onChange={(e) => {
+                              setModel({
+                                ...model,
+                                PhoneNumber: e.target.value,
+                              });
+                              formik.setFieldValue("PhoneNumber", e.target.value);
+                            }}
+                          />
+                          {formik.errors.PhoneNumber && (
+                            <span className="text-danger">
+                              {" "}
+                              {formik.errors.PhoneNumber}
+                            </span>
+                          )}
+                        </div>
+                      </div>
+                      <div className="row mb-3">
+                        <label className="col-md-3 col-form-label">Email</label>
+                        <div className="col-md-9">
+                          <input
+                            type="email"
+                            onChange={(e) => {
+                              setModel({
+                                ...model,
+                                Email: e.target.value,
+                              });
+                              formik.setFieldValue("Email", e.target.value);
+                            }}
+                            className="form-control"
+                          />
+                          {formik.errors.Email && (
+                            <span className="text-danger">
+                              {" "}
+                              {formik.errors.Email}
+                            </span>
+                          )}
+                        </div>
+                      </div>
+                      <div className="row mb-3">
+                        <label className="col-md-3 col-form-label">Web</label>
+                        <div className="col-md-9">
+                          <input
+                            type="text"
+                            onChange={(e) => {
+                              setModel({
+                                ...model,
+                                Web: e.target.value,
+                              });
+                              formik.setFieldValue("Web", e.target.value);
+                            }}
+                            className="form-control"
+                          />
+                          {formik.errors.Web && (
+                            <span className="text-danger">
+                              {" "}
+                              {formik.errors.Web}
+                            </span>
+                          )}
+                        </div>
+                      </div>
+                      <div className="row mb-3">
+                        <label className="col-md-3 col-form-label">
+                          Documents
+                        </label>
+                        <div className="col-md-9">
+                          <input
+                            type="text"
+                            onChange={(e) => {
+                              setModel({
+                                ...model,
+                                Documents: e.target.value,
+                              });
+                              formik.setFieldValue("Documents", e.target.value);
+                            }}
+                            className="form-control"
+                          />
+                          {formik.errors.Documents && (
+                            <span className="text-danger">
+                              {" "}
+                              {formik.errors.Documents}
+                            </span>
+                          )}
                         </div>
                       </div>
                     </div>{" "}
@@ -173,18 +281,23 @@ export default function CreateAgencies() {
                 </div>
                 <ul className="list-inline mb-0 wizard">
                   <Link
-                    to='/agencies'
-                    class="btn btn-danger waves-effect waves-light float-start"
+                    to="/agencies"
+                    className="btn btn-danger waves-effect waves-light float-start"
                   >
-                    <span class="btn-label">
-                      <i class="fe-arrow-left"></i>
+                    <span className="btn-label">
+                      <i className="fe-arrow-left"></i>
                     </span>
                     Anulo
                   </Link>
                   <li className="next list-inline-item float-end">
-                    <button type="submit" className="btn btn-success waves-effect waves-light">
-                    <span class="btn-label"><i class="fe-check"></i></span>
-                    Ruaj
+                    <button
+                      type="submit"
+                      className="btn btn-success waves-effect waves-light"
+                    >
+                      <span className="btn-label">
+                        <i className="fe-check"></i>
+                      </span>
+                      Ruaj
                     </button>
                   </li>
                 </ul>
